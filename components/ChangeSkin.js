0232c621ae495243efbe18dadab3ef2266cecfe7
Olivier <oli.fotboll@live.com>
2021-10-12 10:07:45 +0200

    Next commit

--- components/ChangeSkin.js
+++ components/ChangeSkin.js
@@ -1,30 +1,49 @@
 import React from 'react'
 import { View, Image, StyleSheet } from 'react-native'
 
-export default function ChangeSkin({indexHat = 1, indexHead, indexSweater, indexTrouser}) {
-    let index = 1;
-    const ChangeHat = (indexHat) => {
-        return "../assets/skins/hats/hat"+{indexHat}+".png";
-    } 
-
-    const ChangeHead = (indexHead) => {
-        return "../assets/skins/hats/head"+{indexHead}+".png";
+export default function ChangeSkin({indexHat, indexHead, indexSweater, indexTrouser}) {
+    
+    let index = 1, index1 = 1, index2 = 1, index3 = 1;
+    
+    if (indexHat >  index) {
+        index = indexHat;
+    }
+    if (indexHead >  index1) {
+        index1 = indexHead;
+    }
+    if (indexSweater >  index2) {
+        index2 = indexSweater;
+    }
+    if (indexTrouser >  index3) {
+        index3 = indexTrouser;
     }
 
-    const ChangeSweater = (indexSweater) => {
-        return "../assets/skins/hats/cloth"+{indexSweater}+".png";
-    }
-    const ChangeTrouser = (indexTrouser) => {
-        return "../assets/skins/hats/pants"+{indexTrouser}+".png";
-    }
 
-    return (
-        
+
+    return (    
         <>
             <View>   
                 <Image 
-                    source={require( '../assets/skins/hats/hat'+indexHat+'.png')} 
+                    source={require( '../assets/skins/hats/hat'+index+'.png')} 
                     style={styles.hat}
+                ></Image>
+            </View>
+            <View>   
+                <Image 
+                    source={require( '../assets/skins/heads/head'+index1+'.png')} 
+                    styles={styles.head}
+                ></Image>
+            </View>
+             <View>   
+                <Image 
+                    source={require( '../assets/skins/sweaters/cloth'+index2+'.png')} 
+                    styles={styles.sweater}
+                ></Image>
+            </View>
+            <View>   
+                <Image 
+                    source={require( '../assets/skins/trousers/pants'+index3+'.png')} 
+                    styles={styles.trouser}
                 ></Image>
             </View>
             
@@ -33,11 +52,27 @@ export default function ChangeSkin({indexHat = 1, indexHead, indexSweater, index
 }
 
 const styles = StyleSheet.create({
-    container: {
-      paddingTop: 50,
-    },
     hat: {
-      width: 66,
-      height: 58,
+        alignItem: 'center',
+        paddingTop: 10,
+        justifyContent: 10,
     },
+    head: {
+        alignItems: 'center',
+        paddingTop: 15,
+        justifyContent: 10,
+    
+    },
+    sweater: {
+        alignItems: 'center',
+        paddingTop: 20,
+        justifyContent: 10,
+
+    },
+    trouser: {
+        alignItems: 'center',
+        paddingTop: 30,
+        justifyContent: 10,
+
+    }
   });
--- components/UpdateSkin.js
+++ components/UpdateSkin.js
@@ -1,15 +1,56 @@
 import React from 'react'
+import { Touchable } from 'react-native'
 import ChangeSkin from './ChangeSkin'
+import Background from './children/Background'
 
 export default function UpdateSkin() {
     const [indexHat, setIndexHat] = useState(1)
     const [indexHead, setIndexHead] = useState(1)
     const [indexSweater, setIndexSweater] = useState(1)
-    const [indextrouser, setIndexTrouser] = useState(1)
+    const [indexTrouser, setIndexTrouser] = useState(1)
 
     return (
-        <div>
-            
-        </div>
+        <>
+            <Background/>
+            <View style={styles.container} source={require('../assets/skins/canvas/canvas.png')}></View>
+            <View>
+                <Image style={styles.dude} source={require('../assets/skins/dude/main1.png')}/>
+                <View>
+                    <TouchableNativeFeedback onPress={()=> setIndexHat(indexHat + 1)} background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
+                        <Image style={[styles.rightButton, {paddingTop: 10}]} source={require('../assets/skins/buttons/right.png')}/>
+                    </TouchableNativeFeedback>
+                        <View>
+                            <ChangeSkin indexHat = {indexHat}/>
+                        </View>
+                    <TouchableNativeFeedback onPress={()=> setIndexHat(indexHat - 1)} background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
+                        <Image style={[styles.leftButton, {paddingTop: 10}]} source={require('../assets/skins/buttons/left.png')}/>
+                    </TouchableNativeFeedback>
+                </View>
+            </View>
+        </>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItem: 'center',
+        marginTop: 20,
+
+      },
+      dude: {
+        justifyContent: 'center',
+        alignItem: 'center',
+        marginTop: 10,
+      },
+      rightButton: {
+        alignItem: 'right',
+        justifyContent: 10,
+    },
+    leftButton: {
+        alignItems: 'left',
+        justifyContent: 10,
+    
+    },
+
+})
